b9695cef52afdbe3689e40203ed8ade9eb68a770
Sanaei <iabkriman.sanae@gmail.com>
2018-09-05 12:38:31 +0200

    projet header done

---
+++ resources/views/partials/about.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{url('css/app.css')}}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <title>About me</title>
+</head>
+
+<body>
+<div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="left col-5">
+            <img src="/images/about-2.jpeg" alt="">
+
+            </div>
+            <div class="right col-7">
+                <h3>About me</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil temporibus maxime id, aliquid accusantium eum reiciendis iste. Doloribus, autem corporis! Tempora, error ratione! Quaerat modi quia debitis sint beatae voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore beatae tempore itaque aspernatur facilis in perspiciatis molestiae. Porro eius possimus doloribus sapiente vel exercitationem nam deserunt odio, eum vitae sunt.
+                Doloribus adipisci blanditiis, ullam ipsum aut repudiandae officia a delectus maiores debitis saepe mollitia expedita totam sed facere voluptate recusandae ducimus sapiente perspiciatis? Fugit quibusdam magnam quos quisquam nesciunt tenetur.
+                Accusamus perferendis libero voluptas totam, alias incidunt dolores eum facere cum! Delectus, quaerat rem ullam, reprehenderit obcaecati labore dolore quibusdam quas doloremque quasi hic quisquam! Dolor harum autem dicta sint.
+                Veniam ad eligendi laboriosam dolore sapiente repudiandae id eos voluptatem consectetur? Reiciendis eveniet repellat quis omnis iste expedita harum, odio, similique maxime consequuntur dolores facere culpa, quaerat tenetur soluta! Dolore!
+                Excepturi, eligendi placeat? Alias quos laudantium saepe deleniti labore aut numquam ea, nostrum sequi quam! Iusto nemo nisi alias, assumenda eveniet quasi delectus id laborum blanditiis iste corporis recusandae aliquam! </p>
+                
+            </div>
+        </div>
+    </div>
+</div>
+    
+</body>
+
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{url('css/app.css')}}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <title>HEADER</title>
+
+    <!-- As a heading -->
+    <nav class="navbar navbar-light ">
+        <a class="navbar-brand text-light" href="#">Sanae </a>
+        <div class="links">
+            <a href="">Start</a>
+            <a href="">About me</a>
+            <a href="">Service</a>
+            <a href="">Portfolio</a>
+            <a href="">Blog</a>
+            <a href="">Contact</a>
+        </div>
+    </nav>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md text-white">
+                 <span>Hey </span><span class="font-weight-bold">You !</span>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
